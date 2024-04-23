bbc903e01cec36f5a40aeaddf3e14ba7de415b56
Emily Perez-Rodriguez <emilyp.rodriguez632@gmail.com>
2024-04-22 20:02:12 -0400

    updatd set_value and started incr_value

--- incr_value.cpp
+++ incr_value.cpp
@@ -1,4 +1,26 @@
+#include "csapp.h"
 #include <iostream>
+
+
+void handleResponse(int fd, rio_t* rio,std::string& response_type, const std::string& errorMessage) {
+  char buf[1024];
+    ssize_t n = rio_readlineb(rio, buf, sizeof(buf));
+    if (n <= 0) {
+        std::cerr << "Error: " << errorMessage << std::endl;
+        close(fd);
+        exit(1);
+    }
+    buf[n] = '\0';
+    response_type = buf;
+    if (response_type.find("ERROR") != std::string::npos || response_type.find("FAILED") != std::string::npos) {
+        std::cerr << "Error: "
+                  << response_type.substr(response_type.find('"') + 1,
+                                     response_type.find_last_of('"') - response_type.find('"') - 1)
+                  << "\n";
+        close(fd);
+        exit(1);
+    }
+}
 
 int main(int argc, char **argv) {
   if ( argc != 6 && (argc != 7 || std::string(argv[1]) != "-t") ) {
@@ -23,4 +45,96 @@ int main(int argc, char **argv) {
   std::string key = argv[count++];
 
   // TODO: implement
+  
+  const char *hostname_charstr = hostname.c_str();
+  const char *port_charstr = port.c_str();
+  int fd = open_clientfd(hostname_charstr, port_charstr);
+  if (fd < 0) {
+    std::cerr << "Error: Could not connect to server";
+    return 1;
+  }
+
+  rio_t rio;
+  rio_readinitb(&rio, fd);
+  char buf[1024];
+  ssize_t n;
+
+  if (use_transaction) {
+    // BEGIN
+    rio_writen(fd, "BEGIN\n", 6);
+    // read response from server
+
+    n = rio_readlineb(&rio, buf, sizeof(buf));
+    std::string response(buf);
+    handleResponse(fd, &rio, response, "Failed to receive BEGIN response from server");
+  }
+
+  // send messages to server
+  //  LOGIN
+  rio_writen(fd, ("LOGIN " + username + "\n").c_str(), username.length() + 7);
+
+  // check response from server
+  std::string response_login;
+  handleResponse(fd, &rio, response_login, "Failed to receive LOGIN response from server");
+
+
+  // GET
+  rio_writen(fd, ("GET " + table + " " + key + "\n").c_str(),
+             table.length() + key.length() + 6);
+  // check response from server
+  std::string response_get;
+  handleResponse(fd, &rio, response_get, "Failed to receive GET response from server");
+
+
+  
+  // PUSH (value)
+  rio_writen(fd, "PUSH 1\n", 7);
+
+  // Read response from server after PUSH
+
+  std::string response_push;
+  handleResponse(fd, &rio, response_push, "Failed to receive PUSH response from server");
+
+
+
+  //ADD 
+  rio_writen(fd, "ADD\n", 4);
+
+  // Read response from server after ADD
+  std::string response_add;
+  handleResponse(fd, &rio, response_add, "Failed to receive ADD response from server");
+
+
+  // SET
+  rio_writen(fd, ("SET " + table + " " + key + "\n").c_str(),
+             table.length() + key.length() + 6);
+  // check response from server
+  std::string response_set;
+  handleResponse(fd, &rio, response_set, "Failed to receive SET response from server");
+
+
+  // BYE
+  rio_writen(fd, "BYE\n", 4);
+  // read response from server
+  std::string response_bye;
+  handleResponse(fd, &rio, response_bye, "Failed to receive BYE response from server");
+
+
+  if (use_transaction) {
+    //COMMIT
+    rio_writen(fd, "COMMIT\n", 7);
+    // read response from server
+
+    n = rio_readlineb(&rio, buf, sizeof(buf));
+    if (n <= 0) {
+      std::cerr << "Error: Failed to receive response from server\n";
+      close(fd);
+      return 1;
+    }
+  }
+
+  // close connection
+  close(fd);
+
+  return 0;
 }
--- set_value.cpp
+++ set_value.cpp
@@ -1,6 +1,27 @@
 #include "csapp.h"
 #include <iostream>
 
+
+void handleResponse(int fd, rio_t* rio,std::string& response_type, const std::string& errorMessage) {
+  char buf[1024];
+    ssize_t n = rio_readlineb(rio, buf, sizeof(buf));
+    if (n <= 0) {
+        std::cerr << "Error: " << errorMessage << std::endl;
+        close(fd);
+        exit(1);
+    }
+    buf[n] = '\0';
+    response_type = buf;
+    if (response_type.find("ERROR") != std::string::npos || response_type.find("FAILED") != std::string::npos) {
+        std::cerr << "Error: "
+                  << response_type.substr(response_type.find('"') + 1,
+                                     response_type.find_last_of('"') - response_type.find('"') - 1)
+                  << "\n";
+        close(fd);
+        exit(1);
+    }
+}
+
 int main(int argc, char **argv) {
   if (argc != 7) {
     std::cerr << "Usage: ./set_value <hostname> <port> <username> <table> "
@@ -27,94 +48,35 @@ int main(int argc, char **argv) {
 
   // send messages to server
   //  LOGIN
-  rio_writen(fd, ("LOGIN " + username + "\n").c_str(), username.length() + 7);
 
   // check response from server?
   rio_t rio;
   rio_readinitb(&rio, fd);
-  char buf[1024];
-  ssize_t n = rio_readlineb(&rio, buf, sizeof(buf));
-  if (n <= 0) {
-    std::cerr << "Error: Failed to receive response from server\n";
-    close(fd);
-    return 1;
-  }
-  buf[n] = '\0';
-  std::string response_login(buf);
-
-  // Check response for LOGIN
-  if (response_login.find("ERROR") != std::string::npos ||
-      response_login.find("FAILED") != std::string::npos) {
-    std::cerr << "Error: "
-              << response_login.substr(response_login.find('"') + 1,
-                                       response_login.find_last_of('"') -
-                                           response_login.find('"') - 1)
-              << "\n";
-    close(fd);
-    return 1;
-  }
+ 
+  rio_writen(fd, ("LOGIN " + username + "\n").c_str(), username.length() + 7);
+  std::string response_login;
+  handleResponse(fd, &rio, response_login, "Failed to receive LOGIN response from server");
 
   // PUSH (value)
   rio_writen(fd, ("PUSH " + value + "\n").c_str(), value.length() + 6);
 
   // Read response from server after PUSH
-
-  n = rio_readlineb(&rio, buf, sizeof(buf));
-  if (n <= 0) {
-    std::cerr << "Error: Failed to receive response from server\n";
-    close(fd);
-    return 1;
-  }
-  buf[n] = '\0';
-  std::string response_push(buf);
-
-  if (response_push.find("ERROR") != std::string::npos ||
-      response_push.find("FAILED") != std::string::npos) {
-    std::cerr << "Error: "
-              << response_push.substr(response_push.find('"') + 1,
-                                      response_push.find_last_of('"') -
-                                          response_push.find('"') - 1)
-              << "\n";
-    close(fd);
-    return 1;
-  }
+  std::string response_push;
+  handleResponse(fd, &rio, response_push, "Failed to receive PUSH response from server");
 
   // SET
   rio_writen(fd, ("SET " + table + " " + key + "\n").c_str(),
              table.length() + key.length() + 6);
   // check response from server
-
-  n = rio_readlineb(&rio, buf, sizeof(buf));
-  if (n <= 0) {
-    std::cerr << "Error: Failed to receive response from server\n";
-    close(fd);
-    return 1;
-  }
-  buf[n] = '\0';
-  std::string response_set(buf);
-
-  // Check response for SET
-  if (response_set.find("ERROR") != std::string::npos ||
-      response_set.find("FAILED") != std::string::npos) {
-    std::cerr << "Error: "
-              << response_set.substr(response_set.find('"') + 1,
-                                     response_set.find_last_of('"') -
-                                         response_set.find('"') - 1)
-              << "\n";
-    close(fd);
-    return 1;
-  }
-
+  std::string response_set;
+  handleResponse(fd, &rio, response_set, "Failed to receive SET response from server");
+  
   // BYE
   rio_writen(fd, "BYE\n", 4);
   // read response from server
+  std::string response_bye;
+  handleResponse(fd, &rio, response_bye, "Failed to receive BYE response from server");
 
-  n = rio_readlineb(&rio, buf, sizeof(buf));
-  if (n <= 0) {
-    std::cerr << "Error: Failed to receive response from server\n";
-    close(fd);
-    return 1;
-  }
 
   // close connection
   close(fd);
